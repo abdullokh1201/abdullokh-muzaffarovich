40471fea1dfbd82231d8cce668620d5d64b479fa
abdullokh1201 <muzaffarovich1201@gmail.com>
2025-04-09 15:25:21 +0300

    Update hw2.py

--- lesson-2/homework/hw2.py
+++ lesson-2/homework/hw2.py
@@ -1,5 +1,11 @@
-d = 10
-r = d / 2
-p = 3.14
-C = 2 * r * p
-print(C) 
+from datetime import datetime
+
+name = input("Enter your name: ")
+
+year_of_birth = int(input("Enter your year of birth: "))
+
+current_year = datetime.now().year
+
+age = current_year - year_of_birth
+
+print(f"Hello, {name}! You are {age} years old.")
