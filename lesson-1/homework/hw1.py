fbeafb6e39a6e94c97657ee82aff7ae61343feeb
abdullokh1201 <muzaffarovich1201@gmail.com>
2025-05-04 00:32:40 +0300

    Update hw1.py

--- lesson-1/homework/hw1.py
+++ lesson-1/homework/hw1.py
@@ -1,3 +1,35 @@
-a = 6
-P = 4a
-print(P)
+#1
+
+import math
+a = float(input("Enter the number (a): "))
+P = 4*a
+S = a**2
+print(f"The P and S of {a} is: {P} and {S}")
+
+#2
+
+import math
+diameter = float(input('Enter the deameter of the circle'))
+circuference = math.pi * diameter 
+print(f"The circuference of the circle is: {circuference:.2f}")
+
+#3
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+mean = (a + b)/2
+print(f"The mean of {a} {b} is: {mean}")
+
+#4
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+sum_ab = a + b
+product_ab = a * b 
+square_a = a**2
+square_b = b**2
+print(f"Sum of {a} and {b} is: {sum_ab}")
+print(f"product of {a} and {b} is: {product_ab}")
+print(f'square of {a} is: {square_a}')
+print(f'square of {a} is: {square_b}')
